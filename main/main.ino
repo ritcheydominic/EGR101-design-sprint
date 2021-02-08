94443798b7848eef085cfa01e405955ea644aac4
Dominic Ritchey <me@ritcheydominic.com>
2021-02-08 17:23:59 -0500

    Add base LED code
    Written for 3-LED test strip

--- main/main.ino
+++ main/main.ino
@@ -2,27 +2,44 @@
  * Arduino Code for Design Sprint
  * Written by: Trey Howell and Dominic Ritchey
  * 
- * This program is based on the Arduino sketch example provided at https://www.epitran.it/ebayDrive/datasheet/25.pdf
+ * Encoder code is based on the Arduino sketch example provided at https://www.epitran.it/ebayDrive/datasheet/25.pdf
+ * LED code is based on the Arduino sketch example DemoReel100 provided by the FastLED library written by Daniel Garcia
  */
 
-int pinA = 3; // Connected to CLK on KY-040
-int pinB = 4; // Connected to DT on KY-040
+#include <FastLED.h>
+
+FASTLED_USING_NAMESPACE
+
+#define ENCODER_PIN_A 3 // Connected to CLK on KY-040
+#define ENCODER_PIN_B 4 // Connected to DT on KY-040
 //int encoderPosCount = 0;
-int pinALast;
-int aVal;
+int lastEncoderPinAValue;
+int encoderPinAValue;
 //boolean bCW;
+
+#define LED_DATA_PIN 8
+#define LED_BRIGHTNESS 100
+#define NUM_LEDS 3
+#define LED_TYPE    WS2811
+#define LED_COLOR_ORDER GRB
+#define LED_FPS 30
+CRGB leds[NUM_LEDS];
 
 unsigned long handwashingStarted = 0;
 int secondsElapsed = 0;
 boolean washingHands;
 
 void setup() {
-  // Set up pins
-  pinMode(pinA, INPUT);
-  pinMode(pinB, INPUT);
+  // Set up encoder pins
+  pinMode(ENCODER_PIN_A, INPUT);
+  pinMode(ENCODER_PIN_B, INPUT);
+
+  // Set up LEDs
+  FastLED.addLeds<LED_TYPE, LED_DATA_PIN, LED_COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
+  FastLED.setBrightness(LED_BRIGHTNESS);
   
   // Read pin A (whatever state it's in will reflect the last position)
-  pinALast = digitalRead(pinA);
+  lastEncoderPinAValue = digitalRead(ENCODER_PIN_A);
 
   // Set up logging
   Serial.begin(9600);
@@ -30,9 +47,9 @@ void setup() {
 
 void loop() {
   // Grab pin A value
-  aVal = digitalRead(pinA);
+  encoderPinAValue = digitalRead(ENCODER_PIN_A);
   
-  if (aVal != pinALast) { // Knob has rotated
+  if (encoderPinAValue != lastEncoderPinAValue) { // Knob has rotated
     washingHands = true;
     
     // Log time handwashing started
@@ -41,7 +58,7 @@ void loop() {
     }
     
 //    // Determine direction by grabbing pin B value
-//    if (digitalRead(pinB) != aVal) { // Pin A changed first, so we're rotating clockwise
+//    if (digitalRead(ENCODER_PIN_B) != aVal) { // Pin A changed first, so we're rotating clockwise
 //      encoderPosCount++;
 //      bCW = true;
 //    } else { // Pin B changed first, so we're rotating counterclockwise
@@ -59,7 +76,7 @@ void loop() {
 //    Serial.print("Encoder Position: ");
 //    Serial.println(encoderPosCount);
   }
-  pinALast = aVal; // Update last value for pin A
+  lastEncoderPinAValue = encoderPinAValue; // Update last value for pin A
 
   if (washingHands) {
     if (secondsElapsed < (millis() - handwashingStarted) / 1000) { // Second counter is off
@@ -75,6 +92,21 @@ void loop() {
       washingHands = false;
       secondsElapsed = 0;
       handwashingStarted = 0;
+
+      leds[0] = CHSV(0, 0, 0);
+      leds[1] = CHSV(0, 0, 0);
+      leds[2] = CHSV(0, 0, 0);
     }
-  }  
+
+    if (secondsElapsed == 1) {
+      leds[0] = CRGB::White;
+    } else if (secondsElapsed == 2) {
+      leds[1] = CRGB::White;
+    } else if (secondsElapsed == 3) {
+      leds[2] = CRGB::White;
+    }
+  }
+
+  FastLED.show();
+  FastLED.delay(1000 / LED_FPS);
 }
